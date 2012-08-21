6a3c3aff1d25ceceb104c20a5c4feec3f10fa426
Bulkan Evcimen <bulkan@gmail.com>
2012-08-21 17:59:15 +1000

    added another test

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ django-sqlpaginator
 Paginate raw sql queries using LIMIT and OFFSET 
 
 It will also support ORDER BY queries
+
+tests
+=====
+
+Running tests
+
+    nosetests -s --with-coverage --cover-package=sqlpaginator
+
--- tests/sqlpaginator_tests.py
+++ tests/sqlpaginator_tests.py
@@ -8,14 +8,31 @@ from sqlpaginator.paginator import SqlPaginator
 
 
 class SqlPaginatorTests(TestCase):
+    def test_data_in_db(self):
+        ''' test to see if the models that were created for the Chinook
+        db using the inspectdb management command actually return data '''
 
-    def test_basic(self):
+        self.assertTrue(Artist.objects.all().count() > 0)
+
+    def test_sql_query(self):
+        ''' test that sql query created has the appropriate pagination clauses '''
+
         sql = "SELECT DISTINCT(auth_user.id) FROM auth_user WHERE (DATE_PART('YEAR',AGE(auth_user.dob)) > 10)"
-        sql_paginator = SqlPaginator(User, sql)
+        sql_paginator = SqlPaginator(sql, User)
         self.assertTrue('LIMIT' in sql_paginator.sql)
+        self.assertTrue('ORDER' in sql_paginator.sql)
 
-    def test_pagination(self):
-        sql = "select distinct(artistid) from artist"
+    def test_returns_objects(self):
+        sql = "select distinct(artistid) from %s" % Artist._meta.db_table
         sql_paginator = SqlPaginator(sql, Artist, page=1, order_by='artistid')
         artists = sql_paginator.page(1)
         self.assertTrue(len(artists) > 0)
+
+    def test_has_functions(self):
+        sql = "select distinct(artistid) from %s" % Artist._meta.db_table
+        sql_paginator = SqlPaginator(sql, Artist, page=6, order_by='name')
+        artists = sql_paginator.page(6)
+        self.assertEqual([a.name for a in artists.object_list][0], 'Cake')
+        self.assertTrue(len(artists) > 0)
+        self.assertTrue(artists.has_next())
+        self.assertTrue(artists.has_previous())
