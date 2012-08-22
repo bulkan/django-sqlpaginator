2f42bd6d1bcf97311cbd849cee531bcdf97784ee
Bulkan Evcimen <bulkan@gmail.com>
2012-08-22 22:18:56 +1000

    Updated description

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from setuptools import setup
 
 from version import __version__
 
+description = '''django app that does pagination and ordering using raw
+sql on a Model. It has the same API as the django.core.pagination.Paginator'''
+
 setup(
     name='django-sqlpaginator',
     packages=['sqlpaginator'],
     version=__version__,
-    description='django app that does pagination on using raw sql on a Model',
+    description=description,
     author='Bulkan Evcimen',
     author_email='bulkan@gmail.com',
     url='https://github.com/bulkan/django-sqlpaginator',
