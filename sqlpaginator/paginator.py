3ff163c9455f94a2d8473275db238d0a5622b6e6
Bulkan Evcimen <bulkan@gmail.com>
2012-08-23 11:47:29 +1000

    sql field validation againts model

--- sqlpaginator/paginator.py
+++ sqlpaginator/paginator.py
@@ -16,24 +16,46 @@ class SqlPaginator(object):
     def __init__(self, initial_sql, model, order_by='id', page=1,
                 count=None, per_page=10, direction='asc'):
 
-        self.per_page = per_page
+        # if these are not an int then fail
+        self.page_num = int(page)
+        self.per_page = int(per_page)
+        try:
+            self._count = int(count)
+        except TypeError:
+            self._count = None
+
+        # need the model class to do some sql validation
         self.model = model
-        self.orphans = 0
+
         self._num_pages = None
-        self._count = count
+        self.orphans = 0
         self.initial_sql = initial_sql
         self.object_list = []
-        self.order_by = order_by
-        self.page_num = page
+
         self.allow_empty_first_page = True
+
+        # resolve the model fields into their names
+        self._model_fields = [f.name for f in model._meta.fields]
+
+        self._db_table = self.model._meta.db_table
+
+        # validate sql fields
+        if order_by not in self._model_fields:
+            raise ValueError("%s does is not a valid column in %s" % (order_by, self._db_table))
+
+        self.order_by = order_by
+
+        self.direction = direction
+        if direction.lower() not in ['asc', 'desc']:
+            self.direction = 'asc'
 
         # dict to resolve the sql template with
         self.d = {'sql': initial_sql,
-             'order_by': order_by,
-             'offset': int(page - 1) * self.per_page,
-             'limit': self.per_page,
-             'direction': direction,
-             }
+                  'order_by': order_by,
+                  'offset': int(page - 1) * self.per_page,
+                  'limit': self.per_page,
+                  'direction': direction,
+                 }
 
         self._tsql = '%(sql)s order by %(order_by)s %(direction)s limit %(limit)d offset %(offset)d'
         self._sql = self._tsql % self.d
@@ -79,14 +101,18 @@ class SqlPaginator(object):
                 raise EmptyPage('That page contains no results')
         return number
 
-    def page(self, number, order_by=None):
+    def page(self, number, order_by=None, direction=None):
         number = self.validate_number(number)
 
-        if not order_by:
+        if not order_by and order_by not in self._model_fields:
             order_by = self.order_by
 
+        if not direction or direction.lower() not in ['asc', 'desc']:
+            direction = self.direction
+
         self.d.update({'offset': (number - 1) * self.per_page,
-                       'order_by': order_by})
+                       'order_by': order_by,
+                       'direction': direction})
 
         logger.debug('count: %d' % self.count)
         logger.debug('num_pages: %d' % self.num_pages)
--- tests/sqlpaginator_tests.py
+++ tests/sqlpaginator_tests.py
@@ -2,12 +2,14 @@ from unittest import TestCase
 
 from django.contrib.auth.models import User
 
-
 from models import Artist, Album
 from sqlpaginator.paginator import SqlPaginator
 
 
 class SqlPaginatorTests(TestCase):
+    album_sql = "select albumid from %s" % Album._meta.db_table
+    artist_sql = "select * from %s" % Artist._meta.db_table
+
     def test_data_in_db(self):
         ''' test to see if the models that were created for the Chinook
         db using the inspectdb management command actually return data '''
@@ -15,7 +17,7 @@ class SqlPaginatorTests(TestCase):
         self.assertTrue(Artist.objects.all().count() > 0)
 
     def test_sql_query(self):
-        ''' test that sql query created has the appropriate pagination clauses '''
+        ''' test sql query created has the appropriate pagination clauses '''
 
         sql = "SELECT DISTINCT(auth_user.id) FROM auth_user WHERE (DATE_PART('YEAR',AGE(auth_user.dob)) > 10)"
         sql_paginator = SqlPaginator(sql, User)
@@ -23,29 +25,30 @@ class SqlPaginatorTests(TestCase):
         self.assertTrue('order' in sql_paginator.sql)
 
     def test_returns_objects(self):
-        sql = "select distinct(artistid) from %s" % Artist._meta.db_table
-        sql_paginator = SqlPaginator(sql, Artist, page=1, order_by='artistid')
+        sql_paginator = SqlPaginator(self.artist_sql, Artist,
+                                     page=1, order_by='artistid')
         artists = sql_paginator.page(1)
         self.assertTrue(len(artists) > 0)
 
     def test_has_functions(self):
-        sql = "select * from %s" % Artist._meta.db_table
-        sql_paginator = SqlPaginator(sql, Artist, page=6, order_by='name', per_page=3)
-        artists = sql_paginator.page(6)
+        paginator = SqlPaginator(self.artist_sql, Artist, page=6,
+                                 order_by='name', per_page=3)
+        artists = paginator.page(6)
         self.assertTrue(len(artists) > 0)
         self.assertTrue(artists.has_next())
         self.assertTrue(artists.has_previous())
 
     def test_order_by(self):
-        sql = "select * from %s" % Artist._meta.db_table
-        sql_paginator = SqlPaginator(sql, Artist, page=6, order_by='artistid')
-        artists = sql_paginator.page(6)
+        paginator = SqlPaginator(self.artist_sql, Artist,
+                                 page=6, order_by='artistid')
+        artists = paginator.page(6)
         self.assertEqual([a.artistid for a in artists.object_list][0], 51)
 
     def test_albums(self):
-        sql = "select * from %s" % Album._meta.db_table
         page = 1
-        paginator = SqlPaginator(sql, Album, page=page, order_by='title')
+        paginator = SqlPaginator(self.album_sql, Album,
+                                 page=page, order_by='title')
+
         albums = paginator.page(page)
         self.assertTrue(len(albums) > 0)
 
@@ -54,13 +57,21 @@ class SqlPaginatorTests(TestCase):
         self.assertTrue(len(albums) > 0)
 
     def test_order_by_direction(self):
-        sql = "select * from %s" % Album._meta.db_table
         page = 1
-        paginator = SqlPaginator(sql, Album, page=page, order_by='albumid')
+        paginator = SqlPaginator(self.album_sql, Album,
+                                 page=page, order_by='albumid')
+
         albums = [a.albumid for a in paginator.page(page)]
         self.assertTrue(len(albums) > 0)
         self.assertEqual(albums, range(1, 11))
 
-        paginator = SqlPaginator(sql, Album, page=page, order_by='albumid', direction='desc')
+        paginator = SqlPaginator(self.album_sql, Album, page=page,
+                                 order_by='albumid', direction='desc')
+
         albums = [a.albumid for a in paginator.page(page)]
         self.assertEqual(albums, list(reversed(range(338, 348))))
+
+    def test_invalid_order_by_column(self):
+        page = 1
+        self.assertRaises(ValueError, SqlPaginator,self.album_sql, Album,
+                                 page=page, order_by='albumid$$$')
